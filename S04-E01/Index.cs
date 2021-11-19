84dd614a8a6f52f2c80c7a2c245c556278c98e5a
Humberto <Humberto@DESKTOP-5SQ6PEB>
2021-11-18 21:48:41 -0300

    Add project files.

---
+++ S04-E01/Index.cs
@@ -0,0 +1,22 @@
+﻿namespace S04_E01
+{
+    public class Index
+    {
+        public static void Main()
+        {
+            Pessoa a = new();
+            Pessoa b = new();
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            a.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            a.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nDados da segunda pessoa:");
+            Console.Write("Nome: ");
+            b.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            b.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pessoa mais velha: " + (a.Idade > b.Idade ? a.Nome : b.Nome));
+        }
+    }
+}
