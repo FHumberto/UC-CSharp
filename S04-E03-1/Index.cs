c66c4dea6344838e6c7fa6e1cafeda9ecff3625e
Humberto Guedes <franciscohguedes@hotmail.com>
2021-11-23 23:36:18 -0300

    Add S04-E03-1

---
+++ S04-E03-1/Index.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace S04_E03_1
+{
+    internal class Index
+    {
+        public static void Main()
+        {
+            Retangulo r = new();
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("AREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ S04-E03-1/Retangulo.cs
@@ -0,0 +1,21 @@
+﻿namespace S04_E03_1
+{
+    internal class Retangulo
+    {
+        public double Largura = 0;
+        public double Altura = 0;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
