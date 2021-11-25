4d305c0066cdcc0b895e5224c1adc1ac074ef596
Humberto Guedes <franciscohguedes@hotmail.com>
2021-11-25 09:54:28 -0300

    Add S04-E04

---
+++ S04-E04/Index.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace S04_E04
+{
+    internal class Index
+    {
+        public static void Main()
+        {
+            Console.Write("Qual é a cotação do dolar? ");
+            double Cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("\nQuandos dólares você vai comprar? ");
+            double Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("\nValor a ser pago em reais = " + ConversorDeMoeda.DolarParaReal(Quantidade, Cotacao).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
