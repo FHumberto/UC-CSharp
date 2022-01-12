9ec9a39044662e4174d246415e710f1ca47af849
Humberto Guedes <franciscohguedes@hotmail.com>
2022-01-12 14:54:24 -0300

    Update S06

---
+++ S06-E02/Index.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace S06_E02
+{
+    internal static class Index
+    {
+        public static void Main()
+        {
+            List<Employee> f = new List<Employee>();
+
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Employee #" + (i + 1));
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string? name = Console.ReadLine();
+                Console.Write("Salary ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+                f.Add(new Employee(id, name, salary));
+            }
+
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int searchId = int.Parse(Console.ReadLine());
+            Employee? emp = f.Find(x => x.Id == searchId); // coloca o membro emcontrado em outro objeto
+            if (emp != null)
+            {
+                Console.Write("Enter de percentage: ");
+                double inc = double.Parse(Console.ReadLine(), CultureInfo.CurrentCulture);
+                emp.IncreaseSalary(inc);
+            }
+            else
+            {
+                Console.WriteLine("This Id does not exist!*");
+            }
+
+            Console.WriteLine("\nUpdated List of employess:");
+            foreach (Employee obj in f)
+            {
+                Console.WriteLine(obj); // imprime os dados do objeto
+            }
+        }
+    }
+}
---
+++ S06-E03/Index.cs
@@ -0,0 +1,48 @@
+﻿namespace S06_E03
+{
+    internal static class Index
+    {
+        public static void Main()
+        {
+            string[] l = Console.ReadLine().Split(' ');
+            int m = int.Parse(l[0]);
+            int n = int.Parse(l[1]);
+            int[,] mat = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                string[] v = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(v[j]);
+                }
+            }
+            int z = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == z)
+                    {
+                        Console.WriteLine("Position " + i + "," + j);
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
